1c452e861c751955701c1b4fc2bf516a5928a0ca
Karol Sitarz <ka.sitarz@gmail.com>
2021-03-27 21:17:18 +0100

    add emotion fix tailwindcss name warn on prettier

---
+++ src/app/App.tsx
@@ -0,0 +1,9 @@
+export const App = () => {
+  return (
+    <div className="w-screen h-screen bg-gradient-to-br from-purple-700 to-green-700 flex items-center justify-center">
+      <h1 className="text-gray-200 font-bold text-3xl">
+        Welcome to React + Typescript + Tailwindcss + Prettier (+ Emotion)
+      </h1>
+    </div>
+  );
+};
